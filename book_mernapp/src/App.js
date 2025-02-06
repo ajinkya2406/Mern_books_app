3d418ba0159587b396ceecdcf6989e0c1de78850
ajju06 <pewdikai.gamer@gmail.com>
2025-02-06 11:26:16 +0530

    Navbar created, footer, Home and Login in progress

--- book_mernapp/src/App.js
+++ book_mernapp/src/App.js
@@ -1,12 +1,31 @@
 
 import './App.css';
+import Home from './screen/Home';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
+import Login from './screen/Login';
+
 
 
 function App() {
   return (
-    <>
-      <div>  </div>
-    </>
+    <Router>
+
+      <div>
+
+       <Routes>
+
+        <Route exact path='/' element={<Home/>}/>
+        <Route exact path='/login' element= {<Login/>}/>
+
+       </Routes>
+
+      </div>
+
+    </Router>
   );
 }
 

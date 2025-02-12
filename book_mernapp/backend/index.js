ff60813e792b9d7d99b256eadefcd726950898d8
ajju06 <pewdikai.gamer@gmail.com>
2025-02-13 02:14:27 +0530

    signup page created and connected it with database

--- book_mernapp/backend/index.js
+++ book_mernapp/backend/index.js
@@ -3,6 +3,13 @@ const app = express()
 const port = 5000
 const mongoDB = require("./db")
 mongoDB();
+app.use((req,res,next)=>{
+    res.setHeader("Access-Control-Allow-Origin","http://localhost:3000");
+    res.header("Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept"
+    );
+    next();
+})
 
 app.get('/', (rq,res)=>{
     res.send('Hello World!')
--- book_mernapp/src/App.js
+++ book_mernapp/src/App.js
@@ -10,6 +10,7 @@ import Login from './screen/Login';
 
 import '../node_modules/bootstrap/dist/js/bootstrap.bundle';
 import '../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js'
+import Signup from './screen/Signup.js';
 
 function App() {
   return (
@@ -21,7 +22,8 @@ function App() {
 
         <Route exact path='/' element={<Home/>}/>
         <Route exact path='/login' element= {<Login/>}/>
-
+        <Route exact path='/createuser' element= {<Signup/>}/>
+        
        </Routes>
 
       </div>
